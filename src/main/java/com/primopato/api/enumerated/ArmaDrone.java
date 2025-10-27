556d9f9036894d24ba66956e3efb36a15671c097
Willian Scaquett <willian.scaquett@cotefacil.com>
2025-10-27 14:33:49 -0300

    Corrigida ortografia de texto de arma

--- src/main/java/com/primopato/api/enumerated/ArmaDrone.java
+++ src/main/java/com/primopato/api/enumerated/ArmaDrone.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public enum ArmaDrone {
     //Cada arma e seu respectivo valor em patocoins
-    CAPSULA_CONGELAMENTO("Capsula de Congelamento", new BigDecimal(350)),
+    CAPSULA_CONGELAMENTO("Cápsula de Congelamento", new BigDecimal(350)),
     AGUA_BENTA("Água Benta", new BigDecimal(10)),
     RAIO_LASER("Raio Laser", new BigDecimal(400)),
     ONDA_CHOQUE("Onda de Choque", new BigDecimal(900)),
